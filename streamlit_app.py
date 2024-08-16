b31fc348b6fa9437cac35b8d7a2763c00c94c83e
Nils Walter <nilswalter@me.com>
2024-08-16 09:14:25 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col
 import requests
+from snowflake.snowpark.functions import col
 
 title = st.text_input("Name Smoothie", "fuck that shit")
 st.write("Name of your Smoothie is", title)
@@ -30,11 +30,13 @@ if ingredients_list:
     ingredients_string = ''
     
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen
-        ingredients_string += ' '
+        ingredients_string += fruit_chosen + ' '
         
-            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-            st.text(fruityvice_response.json(), use_container_width=True)
+        import requests
+        # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        # fv_df = st.text(fruityvice_response.json())
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response. json(), use_container_width=True)
         
     # st.write(ingredients_string)
 
